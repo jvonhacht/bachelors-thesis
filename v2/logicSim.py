fbdfd24e41c77f3f957271232b1c47fbc58ca809
David Johansson <davidjo2@kth.se>
2019-04-17 17:01:33 +0200

    preliminary get_state

--- v2/logicSim.py
+++ v2/logicSim.py
@@ -34,9 +34,14 @@ class LogicSimulator:
         }
         
         # fill lanes with cars
-        for i in range(1, 20):  
-            direction = self.get_random_direction(None)                       
-            self.lanes[direction].straight_right.append(Car(self.get_random_direction(direction), direction))        
+        for i in range(0, 100):  
+            direction = self.get_random_direction(Direction.NONE)  
+            if(randint(0,2) == 1):
+                self.lanes[direction].straight_right.append(Car(self.get_random_direction(direction), direction))                    
+            else:            
+                self.lanes[direction].left.append(Car(self.get_random_direction(direction), direction))                                
+
+        self.reward = 0
                     
     def get_state(self):
         """
@@ -47,6 +52,24 @@ class LogicSimulator:
         list
             state of the simulation as NN input
         """        
+        state = []
+        lane_sizes = []
+
+        # get car amount of lanes
+        for lane in self.lanes:
+            lane_sizes.append(self.lanes[lane].size())
+        
+        # amount of car in max lane
+        max_amount = max(lane_sizes) 
+                        
+        if(max_amount != 0):
+            # calculate car amount in each lane 
+            # relative to maximum lane size
+            for i in range(0, 4):            
+                state.append(float(lane_sizes[i])/max_amount)
+        
+        print(state)
+        return state            
 
     def step(self, action):
         """
@@ -66,6 +89,49 @@ class LogicSimulator:
         bool
             if the simulation is finished or not
         """ 
+        self.reward = 0
+
+        if (action == 0):
+            try:                
+                self.lanes[Direction.NORTH].straight_right.popleft()                        
+            except:
+                pass
+            try:                            
+                self.lanes[Direction.NORTH].left.popleft() 
+            except:
+                pass
+        elif (action == 1):  
+            try:
+                self.lanes[Direction.SOUTH].straight_right.popleft()         
+            except:
+                pass
+            try:
+                self.lanes[Direction.SOUTH].left.popleft()                              
+            except:
+                pass
+        elif (action == 2):
+            try:
+                self.lanes[Direction.WEST].straight_right.popleft()         
+            except:
+                pass
+            try:
+                self.lanes[Direction.WEST].left.popleft()         
+            except:
+                pass
+        elif (action == 3):
+            try:
+                self.lanes[Direction.EAST].straight_right.popleft()         
+            except:
+                pass
+            try:
+                self.lanes[Direction.EAST].left.popleft()        
+            except:
+                pass
+
+        done = False if self.get_car_amount() != 0 else True      
+        
+        return self.get_state(), self.reward, done
+
 
     def get_random_direction(self, dir_from):
         """
@@ -89,6 +155,12 @@ class LogicSimulator:
             pass         
         return random.choice(dirs)
 
+    def get_car_amount(self):
+        return self.lanes[Direction.NORTH].size() + \
+               self.lanes[Direction.SOUTH].size() + \
+               self.lanes[Direction.WEST].size()  + \
+               self.lanes[Direction.EAST].size()
+
 class Car:
     def __init__(self, destination, destination_from):
         self.destination = destination
@@ -98,6 +170,14 @@ if __name__ == "__main__":
     simulator = LogicSimulator(1440)      
     for i in range(1,5):
         print(simulator.lanes[i].size())
+    simulator.get_state()
+
+    done = False
+    while not done:
+        done = simulator.step(randint(0,3))[2]              
+
+    for i in range(1,5):
+        print(simulator.lanes[i].size())    
 
     
     
