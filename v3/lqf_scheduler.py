18f850a1b5bcf401ece1956455712c65950d7508
David Johansson <davidjo2@kth.se>
2019-04-21 16:31:11 +0200

    lqf scheduler implementation added

--- v3/lqf_scheduler.py
+++ v3/lqf_scheduler.py
@@ -5,7 +5,27 @@ class LQFScheduler:
         self.simulator = simulator
 
     def schedule(self):
-        return 0
+        # TODO change rewards
+        self.simulator = simulator
+        largest_car_amount = 0
+        direction = Direction.NONE
+        reward = 0
+
+        for key in self.simulator.lanes:
+            lane = self.simulator.lanes[key]
+            lane_size = lane.size()
+            # check if longest queue
+            if(largest_car_amount < lane_size):                
+                largest_car_amount = lane_size
+                direction = key 
+        
+        success = self.simulator.remove_car(direction, 'left')                
+        if (success != -1):
+            reward += success**2                    
+        success = self.simulator.remove_car(direction, 'straight_left')                
+        if (success != -1):
+            reward += success**2
+        return reward 
 
     def __str__(self):
         return 'LQF scheduler'
